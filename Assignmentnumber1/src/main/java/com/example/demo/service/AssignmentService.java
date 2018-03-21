e94a746a623eff46e5ec337ae32b8bab28ad59fd
truong6295 <36268585+truong6295@users.noreply.github.com>
2018-03-21 11:31:10 +0700

    Add files via upload

--- Assignmentnumber1/src/main/java/com/example/demo/service/AssignmentService.java
+++ Assignmentnumber1/src/main/java/com/example/demo/service/AssignmentService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.dao.AssignmentRepository;
 import com.example.demo.dao.PersonalRepository;
